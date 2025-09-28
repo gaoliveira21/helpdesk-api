d4231c01a13b42996b00b0933eb925631fdd54e3
Gabriel Jose dee Oliveira <gabdev.pro@gmail.com>
2025-09-28 12:00:58 -0300

    refactor(application): remove admin creation

--- src/@core/adapters/repositories/in_memory/admin.repository.ts
+++ src/@core/adapters/repositories/in_memory/admin.repository.ts
@@ -4,13 +4,11 @@ import { AdminEntity } from 'src/@core/domain/entities';
 export class InMemoryAdminRepository implements AdminRepository {
   private admins = new Map<string, AdminEntity>();
 
-  constructor(initialAdmins: AdminEntity[] = []) {
-    initialAdmins.forEach((admin) =>
-      this.admins.set(admin.id.toString(), admin),
-    );
-  }
-
   async findById(id: string): Promise<AdminEntity | null> {
     return this.admins.get(id) ?? null;
+  }
+
+  async save(admin: AdminEntity): Promise<void> {
+    this.admins.set(admin.id.toString(), admin);
   }
 }
--- src/@core/application/usecases/create_technician.usecase.spec.ts
+++ src/@core/application/usecases/create_technician.usecase.spec.ts
@@ -1,5 +1,3 @@
-import { AdminEntity } from 'src/@core/domain/entities';
-import { Uuid } from 'src/@core/domain/value_objects';
 import { CreateTechnicianInput } from 'src/@core/domain/usecases/create_technician.usecase';
 import { InMemoryAdminRepository } from 'src/@core/adapters/repositories/in_memory';
 
@@ -7,31 +5,25 @@ import { CreateTechnician } from './create_technician.usecase';
 
 describe('CreateTechnicianUseCase', () => {
   const createUseCase = () => {
-    const admin = AdminEntity.restore({
-      id: new Uuid().toString(),
-      name: 'Admin User',
-      email: 'admin@example.com',
-      passwordHash: 'hasedpassword',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    const adminRepository = new InMemoryAdminRepository([admin]);
+    const adminRepository = new InMemoryAdminRepository();
     const useCase = new CreateTechnician(adminRepository);
 
-    return { admin, useCase, adminRepository };
+    return { useCase, adminRepository };
   };
 
   it('should throw an error if admin is not found', async () => {
-    const { admin, useCase, adminRepository } = createUseCase();
+    const { useCase, adminRepository } = createUseCase();
     adminRepository.findById = jest.fn().mockResolvedValueOnce(null);
     const input: CreateTechnicianInput = {
       name: 'John Doe',
       email: 'john.doe@example.com',
       password: 'securepassword',
-      adminId: admin.id.toString(),
+      adminId: 'non-existent-admin-id',
     };
 
     await expect(useCase.execute(input)).rejects.toThrow('Admin not found');
-    expect(adminRepository.findById).toHaveBeenCalledWith(admin.id.toString());
+    expect(adminRepository.findById).toHaveBeenCalledWith(
+      'non-existent-admin-id',
+    );
   });
 });
