4b711852c4383654af9cafa64d111b7f0498c770
Gabriel Jose dee Oliveira <gabdev.pro@gmail.com>
2025-11-08 10:07:30 -0300

    chore(cookies): add cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import cookieParser from 'cookie-parser';
+
+import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
