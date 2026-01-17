09abea80f0117cc71b3856cb5e469c0e9523f39e
Gabriel Jose dee Oliveira <gabdev.pro@gmail.com>
2026-01-17 09:17:52 -0300

    chore(cors): enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGINS?.split(',').map((origin) =>
+      origin.trim(),
+    ),
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
